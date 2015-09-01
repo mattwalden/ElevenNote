0fdd4007dfe71ff75ca630332a341dbb60436292
MattWalden <mattwalden@mattwalden.com>
2015-09-01 14:33:54 -0400

    9/1 Lunch

---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Web.Models
+{
+   public class GuessingGameViewModel
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(20)]
+        [Display(Name = "Your Name")]
+        public String Name { get; set; }
+
+        [Required]
+        [Range(1,10)]
+        [Display(Name="Your Guess")]
+        public int Guess { get; set; }
+
+    }
+}
