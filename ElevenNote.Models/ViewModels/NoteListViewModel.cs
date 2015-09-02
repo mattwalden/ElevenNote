c0fb1545364443c8690ad110b8ffdf2a3da80da9
MattWalden <mattwalden@mattwalden.com>
2015-09-02 13:15:32 -0400

    9-2 Lunch

---
+++ ElevenNote.Models/ViewModels/NoteListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.ViewModels
+{
+    public class NoteListViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name  = "Date Modified")]
+        public DateTime DateCreated { get; set; }
+
+
+        [Display(Name = "DateModified")]
+        public DateTime? DateModified { get; set; }
+
+        public bool IsFavorite { get; set; }
+
+    }
+}
